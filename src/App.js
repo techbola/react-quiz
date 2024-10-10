ae01064e96c7527e5f7bbf3a47f96d617e30c484
Adebola <bola@neegles.com>
2024-10-10 09:32:51 +0100

    install json-server, fetch questions from local db

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import { type } from "@testing-library/user-event/dist/type";
+
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, status: "ready", questions: action.payload };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: "dataReceived", payload: data });
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch({ type: "dataFailed" });
+      });
+  });
+
   return (
-    <div className="App">
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
